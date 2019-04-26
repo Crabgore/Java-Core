42c1cb78d510d8b35c6606815680e2529ab9e330
Crabgore <crabgore.gonna.hate@gmail.com>
2019-04-26 12:50:44 +0200

    1

--- Homework_1/Main.java
+++ Homework_1/Main.java
@@ -12,19 +12,5 @@ public class Main {
         Team team = new Team("Winners", new Human("Боб"), new Cat("Барсик"), new Cat("Василий"), new Dog("Бобик")); // Создаем команду
         c.doIt(team); // Просим команду пройти полосу
         team.showResults(); // Показываем результаты
-
-
-
-//        Competitor[] competitors = {new Human("Боб"), new Cat("Барсик"), new Dog("Бобик")};
-//        Obstacle[] course = {new Cross(80), new Wall(2), new Wall(1), new Cross(120)};
-//        for (Competitor c : competitors) {
-//            for (Obstacle o : course) {
-//                o.doIt(c);
-//                if (!c.isOnDistance()) break;
-//            }
-//        }
-//        for (Competitor c : competitors) {
-//            c.info();
-//        }
     }
 }
